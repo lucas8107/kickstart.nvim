ac9c6c0fb57c1efe194793cfbcb4b2841a2897d9
Lucas Paula <luolcami@gmail.com>
2024-03-27 15:25:43 +0000

    Diagnostics icons
    Added icons to inline nvim diagnostics

--- init.lua
+++ init.lua
@@ -705,5 +705,11 @@ vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix
 vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
 vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
 
+local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
